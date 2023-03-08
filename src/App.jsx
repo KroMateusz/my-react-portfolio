c32720af8b04ef0a6a328e84538e6f45c6592022
Mateusz Krochmal <k.mateusz1209@gmail.com>
2023-03-08 02:10:10 +0100

    creating home section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Projects from './pages/Projects.jsx';
 import Experience from './pages/Experience.jsx';
+import Contact from './pages/Contact.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 
@@ -15,6 +16,7 @@ function App() {
 					<Route path='/' element={<Home />} />
 					<Route path='/projects' element={<Projects />} />
 					<Route path='/experience' element={<Experience />} />
+					<Route path='contact' element={<Contact />} />
 				</Routes>
 				<Footer />
 			</Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div className='home'>
+			<div className='about'>
+				<h2>Hi! My name is Mateusz</h2>
+				<div className='prompt'>
+					<p>
+						As a Frontend Developer, I have a very good knowledge of
+						HTML, CSS, and JavaScript languages as well as libraries
+						and frameworks such as React or Vue.js.
+					</p>
+					<GitHubIcon />
+					<LinkedInIcon />
+					<EmailIcon />
+				</div>
+			</div>
+			<div className='skills'>
+				<h1>Skills</h1>
+				<ol className='skills-list'>
+					<li className='item'>
+						<h2>Frontend</h2>
+						<span></span>
+					</li>
+				</ol>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
