e8d837489b52c4ba60f4565f4861bfde437f67ce
Mateusz Krochmal <k.mateusz1209@gmail.com>
2023-03-10 02:46:14 +0100

    Creating an experience section

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -19,9 +19,80 @@ function Experience() {
 					icon={<SchoolIcon />}
 				>
 					<h3 className='vertical-timeline-element-title'>
-						My random High School, Random Place, Random State
+						One day in September, I decided to expand my knowledge
+						in programming. I explored different areas of
+						programming and decided to focus on frontend
+						development.
 					</h3>
-					<p>High School Diploma</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--education'
+					date='2020'
+					iconStyle={{ background: '#1f3d9d', color: '#fff' }}
+					icon={<SchoolIcon />}
+				>
+					<h3 className='vertical-timeline-element-title'>
+						Complete the basic courses of HTML, Bootstrap CSS, LESS,
+						SASS on Udemy and Codecademy. Writing
+					</h3>
+					<ul>
+						<p>
+							Web developer od podstaw w 15 dni - Samuraj
+							Programowania
+						</p>
+						<p>
+							Front-end zaawansowany w 15 dni - Samuraj
+							Programowania
+						</p>
+					</ul>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--education'
+					date='2020'
+					iconStyle={{ background: '#1f3d9d', color: '#fff' }}
+					icon={<SchoolIcon />}
+				>
+					<h3 className='vertical-timeline-element-title'>
+						First steps on GitHub and JavaScript and finishing the
+						courses on Udemy. First projects using HTML, CSS, JS and
+						pushing code to GitHub. Webpack, ESLint, Npm, Yarn.
+					</h3>
+					<p>Wprowadzenie do Git i GitHub - Samuraj Programowania</p>
+					<p>Programowanie w JavaScript - Samuraj Programowania</p>
+					<p>
+						Zaawansowane projekty w CSS i JavaScript - Samuraj
+						Programowania
+					</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--education'
+					date='2021'
+					iconStyle={{ background: '#1f3d9d', color: '#fff' }}
+					icon={<SchoolIcon />}
+				>
+					<h3 className='vertical-timeline-element-title'>
+						Writing my first code using React framework. JSX, Props,
+						Hooks, Lifecycle of Components, React Router
+					</h3>
+					<p>
+						React od podstaw - Samuraj Programowania, Mateusz
+						Domanski
+					</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className='vertical-timeline-element--education'
+					date='2022'
+					iconStyle={{ background: '#1f3d9d', color: '#fff' }}
+					icon={<SchoolIcon />}
+				>
+					<h3 className='vertical-timeline-element-title'>
+						Creating first applications in Vue.js. Component
+						Communication, Http Requests, Vuex, Composition API
+					</h3>
+					<p>
+						VUe - The Complete Guide (incl. Ruter & Composition API)
+						- Maximilian Schwarzmuller
+					</p>
 				</VerticalTimelineElement>
 			</VerticalTimeline>
 		</div>
